12748f3fc6d81577530c9c905fd43dca672b3b0a
101579898 <245239783+101579898@users.noreply.github.com>
2026-02-28 23:04:29 +0000

    sighh

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ const synth = new Tone.Synth().toDestination();
 function playTrack() {
     synth.triggerAttackRelease("A4", "8n");
 }
-
-// all your button listeners
-document.getElementById("btnPlay").addEventListener("click", playTrack);
-
-// all your animation logic
-function spinVinyl() {
-    document.querySelector(".vinyl").classList.add("playing");
-}
